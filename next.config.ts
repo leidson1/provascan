0a802496bf3bc73f1be62d31343509c3d74add50
Leidson <leidson.lima@gmail.com>
2026-05-12 15:53:42 -0300

    Permite acesso local ao dev server pelo celular

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { networkInterfaces } from "node:os";
+
+const localNetworkHosts = Object.values(networkInterfaces())
+  .flatMap((interfaces) => interfaces ?? [])
+  .filter((networkInterface) => networkInterface.family === "IPv4" && !networkInterface.internal)
+  .map((networkInterface) => networkInterface.address);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: localNetworkHosts,
 };
 
 export default nextConfig;
